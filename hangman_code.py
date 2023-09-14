31a9947d3003293b6a54139d385df4a6bb9b5251
Bhaskar Jha <jhabhaskar950@gmail.com>
2023-09-14 22:31:25 +0530

    Hangman Game implemented using Python

---
+++ hangman_code.py
@@ -0,0 +1,33 @@
+import random
+import word_file
+import hanging_man
+health=6
+chosen_word=random.choice(word_file.words)
+print(chosen_word)
+display=[]
+for i in range(len(chosen_word)):
+    display += '_'
+print(display)
+game_over=False
+while not game_over:
+    gussed_letter=input("Guess a letter \n").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == gussed_letter:
+            display[position]=gussed_letter
+    print(display)        
+
+    if gussed_letter not in chosen_word:
+        health -= 1
+        if health == 0:
+            game_over=True
+            print("You Lose !!")
+    if '_' not in display:
+        game_over = True
+        print("You Win !!")
+    print(hanging_man.hangman_stages[health])
+
+
+
+
+
---
+++ word_file.py
@@ -0,0 +1,5 @@
+words = [
+   
+    "apple", "banana", "cherry", "date", "grape", "jackfruit", "kiwi", "lemon", "mango", "orange",
+    "pineapple", "raspberry", "strawberry", "watermelon", "bear", "cat", "dog", "elephant", "fox",
+    "giraffe", "hippopotamus", "jaguar", "kangaroo", "lion","monkey", "octopus", "penguin", ]
